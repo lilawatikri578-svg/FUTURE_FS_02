e914fb0aae9a8807c7e940e16f8d58e814eaeadd
Lilawati Kumari Yadav <lilawatikri578@gmail.com>
2026-07-23 06:35:27 +0530

    Add files via upload

---
+++ Library-CRM/config/db.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "library_crm"
+});
+
+connection.connect((err) => {
+    if(err){
+        console.log("Database Connection Failed!");
+        console.log(err);
+        return;
+    }
+
+    console.log("MySQL Connected Successfully!");
+});
+
+module.exports = connection;
+
